aa553eb88c3be70f33232ec7ec350ffa741c9048
Kyrylo Alieksieiev <alieksieiev@Sadie.local>
2024-10-26 17:00:15 +0200

    Connected C++ side, I can peek any piece and it will highlight and black can move according to rules

--- src/BoardPanel.java
+++ src/BoardPanel.java
@@ -1,39 +1,132 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.util.Arrays;
 
-public class BoardPanel extends JPanel{
+
+public class BoardPanel extends JPanel implements MouseListener{
+
+    boolean pieceSelected = false;
+
+    static {
+        System.load("/Users/alieksieiev/CLionProjects/Checkers/cmake-build-debug/libCheckers.dylib");
+    }
 
     private static final int BOARD_SIZE = 8;
-    private static final int DEFAULT_TILE_SIZE = 1;
-    boolean bw = true;
+    CheckersJNI jni = new CheckersJNI();
+    int selectedRow = -1;
+    int selectedCol = -1;
+
 
     public BoardPanel() {
         setFocusable(true);
-        setPreferredSize(new Dimension(BOARD_SIZE * DEFAULT_TILE_SIZE, BOARD_SIZE * DEFAULT_TILE_SIZE));
+        addMouseListener(this);
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
+        paintBoard(g);
+        paintPieces(g);
+    }
+
+    public void paintBoard(Graphics g){
         int squareSizeX = getWidth() / 8;
         int squareSizeY = getHeight() / 8;
 
-        for(int i=0, x=0; i<BOARD_SIZE; i++, x+=squareSizeX){
-            for(int j=0, y=0; j<BOARD_SIZE; j++, y+=squareSizeY){
-                if((i+j)%2==0) {
-                    g.setColor(new Color(236, 219, 185));
-                } else {
-                    g.setColor(new Color(174,137,104));
-                }
+        for(int i=0, x=0; i<8; i++, x+=squareSizeX){
+            for(int j=0, y=0; j<8; j++, y+=squareSizeY){
+                boolean isDarkSquare = (j + i) % 2 == 1;
+
+                // Подсветка выбранной клетки
+                if (j == selectedRow && i == selectedCol)
+                    g.setColor(isDarkSquare ? new Color(168,182,88) : new Color(201, 223, 126));
+                else
+                    g.setColor(isDarkSquare ? new Color(174,137,104) : new Color(236, 219, 185));
+
                 g.fillRect(x, y, squareSizeX, squareSizeY);
             }
         }
     }
 
-    public void paintPieces(Graphics g, int[][] boardState){
+    public void paintPieces(Graphics g){
+
+        int squareSizeX = getWidth() / 8;
+        int squareSizeY = getHeight() / 8;
+
+        for(int i=0, x=0; i<BOARD_SIZE; i++, x+=squareSizeX){
+            for(int j=0, y=0; j<BOARD_SIZE; j++, y+=squareSizeY){
+                if (jni.getBoardState()[j * 8 + i] == 1) {
+                    g.setColor(Color.BLACK);
+                    g.fillOval(x + (squareSizeX - squareSizeX + 10) / 2,
+                               y + (squareSizeY - squareSizeY + 10) / 2,
+                            squareSizeX - 10,
+                            squareSizeY - 10);
+                }
+                else if (jni.getBoardState()[j * 8 + i] == 2) {
+                    g.setColor(Color.WHITE);
+                    g.fillOval(x + (squareSizeX - squareSizeX+10) / 2,
+                               y + (squareSizeY - squareSizeY+10) / 2,
+                            squareSizeX-10,
+                            squareSizeY-10);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        
+        int TILE_SIZE_X = getWidth() / 8;
+        int TILE_SIZE_Y = getHeight() / 8;
+
+        int col = e.getX() / TILE_SIZE_X;
+        int row = e.getY() / TILE_SIZE_Y;
+        int index = row * 8 + col;
+
+
+        if (!pieceSelected) {
+            //First click
+            if (jni.getBoardState()[index] != 0) {  // Проверяем, что фишка существует
+                selectedRow = row;
+                selectedCol = col;
+                pieceSelected = true;  // Фиксируем, что фишка выбрана
+            }
+        } else {
+
+            int toRow = row;
+            int toCol = col;
+
+            // Вызываем метод movePiece в C++ через JNI, передавая координаты
+            boolean validMove = jni.movePiece(selectedRow, selectedCol, toRow, toCol);
+
+
+            // Сброс выбранной фишки после хода
+            pieceSelected = false;
+            selectedRow = -1;
+            selectedCol = -1;
+        }
+
+        repaint();
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
 
     }
 }
